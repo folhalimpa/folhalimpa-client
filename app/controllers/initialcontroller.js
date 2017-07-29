1af04a4447cce128ba61dbb3ab93920f75622414
yurimalheiros <yurimalheiros@gmail.com>
2017-07-29 19:00:39 -0300

    corrigindo links na pagina inicial

--- app/controllers/initialcontroller.js
+++ app/controllers/initialcontroller.js
@@ -1,6 +1,6 @@
 folhaLimpaApp.controller('InitialController', ['$scope', function ($scope) {
-    $scope.cases = [{ title: 'Pagamentos na Prefeitura de João Pessoa', img: 'joaopessoa', href: 'ugestora.html' },
-    { title: 'Pagamentos na Prefeitura de Campina Grande', img: 'campinagrande', href: 'servidor.html' },
-    { title: 'Pagamentos na Prefeitura de Bayeux', img: 'bayeux', href: 'ugestora.html' },
-    { title: 'Pagamentos na Prefeitura de Lucena', img: 'lucena', href: '/#!/unidade/21/7/2016/7/2017' }];
+    $scope.cases = [{ title: 'Pagamentos na Prefeitura de João Pessoa', img: 'joaopessoa', href: '/#!/unidade/20/4/2016/4/2017' },
+    { title: 'Pagamentos na Prefeitura de Campina Grande', img: 'campinagrande', href: '/#!/unidade/53/4/2016/4/2017' },
+    { title: 'Pagamentos na Prefeitura de Bayeux', img: 'bayeux', href: '/#!/unidade/87/4/2016/4/2017' },
+    { title: 'Pagamentos na Prefeitura de Lucena', img: 'lucena', href: '/#!/unidade/21/4/2016/4/2017' }];
 }]);
