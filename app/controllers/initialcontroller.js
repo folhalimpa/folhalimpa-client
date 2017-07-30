e1751833c42bc718c79a2bcb57bc72ed38cf5a9a
yurimalheiros <yurimalheiros@gmail.com>
2017-07-30 17:24:57 -0300

    usando pretty urls

--- app/controllers/initialcontroller.js
+++ app/controllers/initialcontroller.js
@@ -1,6 +1,6 @@
 folhaLimpaApp.controller('InitialController', ['$scope', function ($scope) {
-    $scope.cases = [{ title: 'Pagamentos na Prefeitura de João Pessoa', img: 'joaopessoa', href: '/#!/unidade/20/4/2016/4/2017' },
-    { title: 'Pagamentos na Prefeitura de Campina Grande', img: 'campinagrande', href: '/#!/unidade/53/4/2016/4/2017' },
-    { title: 'Pagamentos na Prefeitura de Bayeux', img: 'bayeux', href: '/#!/unidade/87/4/2016/4/2017' },
-    { title: 'Pagamentos na Prefeitura de Lucena', img: 'lucena', href: '/#!/unidade/21/4/2016/4/2017' }];
+    $scope.cases = [{ title: 'Pagamentos na Prefeitura de João Pessoa', img: 'joaopessoa', href: '/unidade/20/4/2016/4/2017' },
+    { title: 'Pagamentos na Prefeitura de Campina Grande', img: 'campinagrande', href: '/unidade/53/4/2016/4/2017' },
+    { title: 'Pagamentos na Prefeitura de Bayeux', img: 'bayeux', href: '/unidade/87/4/2016/4/2017' },
+    { title: 'Pagamentos na Prefeitura de Lucena', img: 'lucena', href: '/unidade/21/4/2016/4/2017' }];
 }]);
